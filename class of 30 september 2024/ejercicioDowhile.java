3ab3f8fc67d287b7847e9654c19077e16b498063
hector10-45 <hminota@estudiante.uniajc.edu.co>
2024-09-30 21:22:28 +0000

    explicacion do while

---
+++ class of 30 september 2024/ejercicioDowhile.java
@@ -0,0 +1,30 @@
+public class ejercicioDowhile  {
+
+    public static void main(String[] args) {
+        /*
+        *  do while 
+        */
+        
+     
+     
+        String cad = "programacion I"; 
+     int j = 1;  
+do {
+System.out.println(cad);
+j++;
+
+}while(j>5);
+
+
+/*
+* while / for
+*/
+System.out.println("*****************************************************************");
+
+for ( int i = 1;i < 5;i++){
+    System.out.println(cad);
+}
+
+    }
+    
+}
