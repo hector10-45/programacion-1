91716d212be1229644db55cda8a150bcd1d7ef7c
hector10-45 <hminota@estudiante.uniajc.edu.co>
2024-09-30 20:36:25 +0000

    finalizacion ejercicio 5.6

--- class of 30 september 2024/ejercicio56.java
+++ class of 30 september 2024/ejercicio56.java
@@ -1,13 +1,37 @@
-public class ejercicio56  {
+import java.util.Scanner;
 
-public static void main(String[] args) {
+public class ejercicio56 {
 
+    public static void main(String[] args) {
 
-    System.out.println("mi primer codigo  en github");
-   
+        // codigo 5.6
+        Scanner leer = new Scanner(System.in);
+        int cantidad;
+        int estudiantes;
+        double nota = 0.0;
+        double promedio;
+        int aprobados = 0;
+        int reprobados = 0;
 
+        System.out.print("digite la cantidad de estudantes ");
+        estudiantes = leer.nextInt();
 
-}
-    
-    
+        for (int i = 1; i <= estudiantes; i++) {
+
+            System.out.print("nota del estudante :   " + i);
+            nota = leer.nextDouble();
+
+            if (nota >= 5.0) {
+
+                aprobados++;
+
+            } else {
+                reprobados++;
+            }
+
+        }
+
+        System.out.print(" aprobaos :   " + aprobados + "   los reprobados son :   " + reprobados);
+    }
+
 }
